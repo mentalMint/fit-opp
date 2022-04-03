d3ab7cef66bd59bc3e6fd9581f38e1758c31ef45
mentalMint <walkershush@gmail.com>
2022-04-04 01:04:02 +0700

    sorted

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <omp.h>
+
+void multiplyMatrix(const double* A, const double* x, double* Ax, int N) {
+#pragma omp single
+    fillZeroArray(Ax, N);
+#pragma omp for SCHEDULE
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            Ax[i] += A[i * N + j] * x[j];
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
     return 0;
 }
