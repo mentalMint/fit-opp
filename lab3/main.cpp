aaba2634ea4da18c7d8d1be6a2659ad5055c7e37
mentalMint <walkershush@gmail.com>
2022-04-04 01:13:02 +0700

    started

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <omp.h>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
 
-void multiplyMatrix(const double* A, const double* x, double* Ax, int N) {
+#define SCHEDULE schedule(runtime)
+
+using namespace std;
+
+void fillZeroArray(double* x, int N) {
+    memset(x, 0, sizeof(double) * N);
+}
+
+void multiplyMatrices(const double* A, const double* B, double* C, int N) {
 #pragma omp single
-    fillZeroArray(Ax, N);
+    fillZeroArray(C, N * N);
 #pragma omp for SCHEDULE
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -12,7 +23,33 @@ void multiplyMatrix(const double* A, const double* x, double* Ax, int N) {
     }
 }
 
+void readInputData(double* A, double* B) {
+    ifstream in("src.txt");
+    int N;
+    in >> N;
+    for (int i = 0; i < N * N; ++i) {
+        in >> A[i];
+    }
+    
+    for (int i = 0; i < N * N; ++i) {
+        in >> B[i];
+    }
+
+    in.close();
+}
+
 int main() {
+    ifstream in("src.txt");
+    int N;
+    in >> N;
+    in.close();
+    double* A = new double[N * N];
+    double* B = new double[N * N];
+    
+    readInputData(A, B);
+    
+    double* C = new double[N * N];
+    multiplyMatrices(A, B, C, N);
     
     return 0;
 }
