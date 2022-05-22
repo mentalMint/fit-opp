efb2806241f0fcb0af428ba0853a54ae762660d1
mentalMint <walkershush@gmail.com>
2022-05-22 21:27:54 +0700

    This sh** doesn't work

---
+++ lab5/Task.h
@@ -0,0 +1,37 @@
+//
+// Created by Roman on 22.05.2022.
+//
+
+#ifndef LAB5_TASK_H
+#define LAB5_TASK_H
+
+
+class Task {
+private:
+    int repeatNum = 0;
+
+public:
+    Task(int repeatNum) : repeatNum(repeatNum) {
+    }
+    
+    Task(){}
+    
+    void setRepeatNum(int repeat_num) {
+        repeatNum = repeat_num;
+    }
+    
+    int getRepeatNum() const {
+        return repeatNum;
+    }
+    
+    double perform() {
+        double sum = 0;
+        for (int i = 0; i < repeatNum; i++) {
+            sum += repeatNum;
+        }
+        return sum;
+    }
+};
+
+
+#endif //LAB5_TASK_H
